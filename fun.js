af4f445b0426abb4b3692da8cf86b1b9073fe490
D. S. Caleidoscope <dscaleidoscope@gmail.com>
2023-10-08 11:57:22 +0200

    working with session

--- fun.js
+++ fun.js
@@ -30,20 +30,22 @@ function getParams(){
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const mr = urlParams.get('mr');
+  const cs = urlParams.get('cs');
   
   maxRead = 64;
   
   if(mr > 0) {
     maxRead = mr;
   }
-  
-  //console.log("MaxRead: " + maxRead);
+
+  if (cs == 1) {
+    localStorage.clear();
+  }
 }
 
 //AUTHORS
 async function parseAuthors(t){
   let authors = JSON.parse(t);
-  //console.log(authors);
   authorMax = authors.Author.length;
   
   let i = 0;
@@ -63,25 +65,26 @@ async function parseAuthors(t){
 function parseSingleAuthor(t){
   let singleAuthor = JSON.parse(t);
 
-  //create methods
-  singleAuthor.Auth.getID = function(){return this.Id;};
-  singleAuthor.Auth.getAuthorName = function(){return this.AuthorName;};
-  singleAuthor.Auth.getName = function(){return this.Name;};
-  singleAuthor.Auth.getGenres = function(){return this.Genres;};
-  singleAuthor.Auth.getPresentation = function(){return this.Presentation;};
-  singleAuthor.Auth.getProfilePic = function(){return this.ProfilePic;};
-  singleAuthor.Auth.addResumedInfo = function(node){
+  //create methods & push it to the array
+  addAuthorMethods(singleAuthor.Auth);
+  authorList.push(singleAuthor.Auth);
+}
+
+function addAuthorMethods(auth) {
+  auth.getID = function () { return this.Id; };
+  auth.getAuthorName = function () { return this.AuthorName; };
+  auth.getName = function () { return this.Name; };
+  auth.getGenres = function () { return this.Genres; };
+  auth.getPresentation = function () { return this.Presentation; };
+  auth.getProfilePic = function () { return this.ProfilePic; };
+  auth.addResumedInfo = function (node) {
     node.innerHTML += " Autor: " + this.getAuthorName();
   }
-  
-  //console.log(singleAuthor.Auth);
-  authorList.push(singleAuthor.Auth);
 }
 
 //BOOKS
 async function parseBooks(t){
   let books = JSON.parse(t);
-  //console.log(books);
   bookMax = books.Book.length;
   
   let i = 0;
@@ -101,19 +104,24 @@ async function parseBooks(t){
 function parseSingleBook(t){
   let singleBook = JSON.parse(t);
   
-  //create methods
-  singleBook.Book.authList = [];
-  singleBook.Book.getID = function(){return this.Id;};
-  singleBook.Book.getTitle = function(){return this.Title;};
-  singleBook.Book.getAuthorsID = function(){return this.Authors;};
-  singleBook.Book.getGenres = function(){return this.Genres;};
-  singleBook.Book.getSynopsis = function(){return this.Synopsis;};
-  singleBook.Book.getCover = function(){return this.Cover;};
-  singleBook.Book.getNetworks = function(){return this.Networks;};
-  singleBook.Book.getASIN = function(){return this.ASIN;};
-  singleBook.Book.createNode = function(id){return document.createElement("div");}
-  singleBook.Book.appendNode = function(node){g('bbody').appendChild(node);}
-  singleBook.Book.addInfo = function(al){
+  //create methods & push it to the array
+  addBookMethods(singleBook.Book);
+  bookList.push(singleBook.Book);
+}
+
+function addBookMethods(book) {
+  book.authList = [];
+  book.getID = function () { return this.Id; };
+  book.getTitle = function () { return this.Title; };
+  book.getAuthorsID = function () { return this.Authors; };
+  book.getGenres = function () { return this.Genres; };
+  book.getSynopsis = function () { return this.Synopsis; };
+  book.getCover = function () { return this.Cover; };
+  book.getNetworks = function () { return this.Networks; };
+  book.getASIN = function () { return this.ASIN; };
+  book.createNode = function (id) { return document.createElement("div"); }
+  book.appendNode = function (node) { g('bbody').appendChild(node); }
+  book.addInfo = function (al) {
     let node = this.createNode(this.getID());
     this.addTitleInfo(node);
     this.addAuthorInfo(node, al);
@@ -124,54 +132,50 @@ function parseSingleBook(t){
     this.addASINInfo(node);
     this.appendNode(node);
 
-    //console.log(this.authList);
     //add("Relations: [" + pri + " / " + bookMax + "]");
   };
 
-  singleBook.Book.addTitleInfo = function(node){
+  book.addTitleInfo = function (node) {
     node.innerHTML += "Titulo: " + this.getTitle();
   }
-  
-  singleBook.Book.addAuthorInfo = function(node, al){
+
+  book.addAuthorInfo = function (node, al) {
     let i = 0;
-    let auth = this.getAuthorsID(); 
+    let auth = this.getAuthorsID();
     let x = auth.length;
 
-    for(;i < x;i++){
+    for (; i < x; i++) {
       this.addSingleAuthor(node, auth[i], al);
     }
   };
 
-  singleBook.Book.addSingleAuthor = function (node, auth, al){
+  book.addSingleAuthor = function (node, auth, al) {
     let i = 0;
     let x = al.length;
 
-    for(;i < x;i++){
-      if(auth == al[i].getID()){
+    for (; i < x; i++) {
+      if (auth == al[i].getID()) {
         this.authList.push(al[i]);
         al[i].addResumedInfo(node);
       }
     }
   };
 
-  singleBook.Book.addGenresInfo = function(node){
+  book.addGenresInfo = function (node) {
     node.innerHTML += " Generos: " + this.getGenres();
   };
-  singleBook.Book.addSynopsisInfo = function(node){
+  book.addSynopsisInfo = function (node) {
     node.innerHTML += " Synopsis: " + this.getSynopsis();
   };
-  singleBook.Book.addCoverInfo = function(node){
+  book.addCoverInfo = function (node) {
     node.innerHTML += " Cubierta: " + this.getCover();
   };
-  singleBook.Book.addNetworksInfo = function(node){
+  book.addNetworksInfo = function (node) {
     node.innerHTML += " Redes: " + this.getNetworks();
   };
-  singleBook.Book.addASINInfo = function(node){
+  book.addASINInfo = function (node) {
     node.innerHTML += " ASIN: " + this.getASIN();
   };
-
-  //console.log(singleBook.Book);
-  bookList.push(singleBook.Book);
 }
 
 //LAUNCHER
@@ -179,56 +183,82 @@ async function fill(){
   let fList = [];
   clear();
 
-  let bp = filePromise("book_master.json").then(
-    function(value) {bookMaster = value},
-    function(error) {console.log(error);}
-  );
+  //check session to pump performance
+  let isDone = localStorage.getItem('datk_done');
 
-  let ap = filePromise("author_master.json").then(
-    function(value) {authorMaster = value},
-    function(error) {console.log(error);}
-  );
+  if (isDone == 1) {
+    //user already loaded the page
+    let brList = [];
+    let arList = [];
+    let ai = 0;
+    let bi = 0;
+    let ax = 0;
+    let bx = 0;
 
-  //put promises into array
-  fList.push(bp);
-  fList.push(ap);
+    add("Session is up!");
+    bookList = JSON.parse(localStorage.getItem('datk_books'));
+    authorList = JSON.parse(localStorage.getItem('datk_authors'));
 
-  //await full completion
-  await Promise.allSettled(fList);
-  
-  /*
-  console.log("Master data read!");
-  console.log("Records to be read: ");
-  console.log("  Authros: " + authorMax);
-  console.log("  Books: " + bookMax);
-  console.log("  Relations: " + bookMax);
-  */
-  
-  add("MaxRead: " + maxRead);
-  
-  //start getting real data until all is done
-  //we're getting only small chunks each time to avoid network issues
-  while(pai < authorMax && pbi < bookMax) {
-    //performance
-    let ini = Date.now();
-    
-    parseBooks(bookMaster);
-    parseAuthors(authorMaster);
+    //restore methods as JSON make them loose
+    ax = authorList.length;
+    bx = bookList.length;
 
-    //then wait for authors and books
-    await Promise.allSettled(aList);
-    await Promise.allSettled(bList);
-    
-    //add("Books: [" + pbi + " / " + bookMax + "]"); 
-    //add("Authors: [" + pai + " / " + authorMax + "]");
-    add("Time: " + (Date.now() - ini));
+    for (; ai < ax; ai++) {
+      let p = restoreAuthPromise(authorList[ai]);
+      arList.push(p);
+    }
+
+    for (; bi < bx; bi++) {
+      let p = restoreBookPromise(bookList[bi]);
+      brList.push(p);
+    }
+
+    //await full completion
+    await Promise.allSettled(arList);
+    await Promise.allSettled(brList);
+  } else {
+    //new user
+    let bp = filePromise("book_master.json").then(
+      function (value) { bookMaster = value },
+      function (error) { console.log(error); }
+    );
+
+    let ap = filePromise("author_master.json").then(
+      function (value) { authorMaster = value },
+      function (error) { console.log(error); }
+    );
+
+    //put promises into array
+    fList.push(bp);
+    fList.push(ap);
+
+    //await full completion
+    await Promise.allSettled(fList);
+
+    add("MaxRead: " + maxRead);
+
+    //start getting real data until all is done
+    //we're getting only small chunks each time to avoid network issues
+    while (pai < authorMax && pbi < bookMax) {
+      //performance (uncomment when needed)
+      //let ini = Date.now();
+
+      parseBooks(bookMaster);
+      parseAuthors(authorMaster);
+
+      //then wait for authors and books
+      await Promise.allSettled(aList);
+      await Promise.allSettled(bList);
+
+      //performance (uncomment when needed)
+      //add("Time: " + (Date.now() - ini));
+    }
+
+    //save data into local storage
+    localStorage.setItem('datk_books', JSON.stringify(bookList));
+    localStorage.setItem('datk_authors', JSON.stringify(authorList));
+    localStorage.setItem('datk_done', 1);
   }
-
-  /*
-  console.log("Add loaded!");
-  console.log("authors: " + authorList);
-  console.log("books: " + bookList);
-  */
 
   //relate & print
   relate();
@@ -239,6 +269,8 @@ async function fill(){
 async function relate(){
   let i = 0;
   let x = bookList.length;
+
+  console.log("x: " + x);
 
   for(;i < x;i++){
     let p = printPromise(bookList[i], authorList).then(
@@ -251,7 +283,6 @@ async function relate(){
 
   //await full completion
   const results = await Promise.allSettled(rList);
-  //console.log("bList: " + results);
 }
 
 //File Reader
@@ -283,3 +314,18 @@ let printPromise = async function(book, authroList) {
 
   return p;
 };
+
+//Restorer
+let restoreBookPromise = async function (book) {
+  let p = new Promise(function (resolve, reject) {
+    addBookMethods(book);
+    resolve(true);
+  });
+}
+
+let restoreAuthPromise = async function (auth) {
+  let p = new Promise(function (resolve, reject) {
+    addAuthorMethods(auth);
+    resolve(true);
+  });
+}
