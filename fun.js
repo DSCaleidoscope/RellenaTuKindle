dbf3c478d2da282af5eccefd5f66d2bc5f468899
D. S. Caleidoscope <dscaleidoscope@gmail.com>
2025-06-22 17:29:21 +0200

    dress rehearsal

--- fun.js
+++ fun.js
@@ -37,8 +37,8 @@ var Bcollapsed = [];
 //Config
 var forceRefresh = true; //During the event, move it to false to avoid forcing refresh!
 var eventDate = "24 de Junio";
-var isEventWaiting = true; //Move it to false to show links to Amazon
-var tracking = false; //Move it to false to avoid calling tracking function
+var isEventWaiting = false; //Move it to false to show links to Amazon
+var tracking = true; //Move it to false to avoid calling tracking function
 
 //helpers & redefines
 function g(id){return document.getElementById(id);}
